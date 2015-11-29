d7ef834d595f8591398fb937a017baa22772b831
Christopher Hoogeboom <chris.hoogeboom@gmail.com>
2015-11-28 19:54:46 -0500

    fixed some bugs

--- hpmor.py
+++ hpmor.py
@@ -1,5 +1,7 @@
-import wepub
+import re
+
 import bs4
+import wepub
 
 
 class HPMoR(wepub.Chapter):
@@ -7,12 +9,15 @@ class HPMoR(wepub.Chapter):
     def __init__(self, url: str, html: str=None):
         super().__init__(url, html)
 
-    def get_title(self) -> bs4.Tag:
-        return self.parsed_html.find(name="div", id="chapter-title")
+    def get_title(self) -> str:
+        raw_title = self.parsed_html.find(name="div", id="chapter-title").text
+        return re.sub(pattern=re.compile(r'\n'), repl=' ', string=raw_title)
 
     def get_content(self) -> bs4.Tag:
         return self.parsed_html.find(id="storycontent")
 
-    def get_next_chapter_url(self):
+    def get_next_chapter_url(self) -> str:
         anchor = self.parsed_html.find("a", string=re.compile("Next"))
-        return self.fix_url(anchor["href"])
+        return None if anchor is None else self.fix_url(anchor["href"])
+
+
