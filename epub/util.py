cde249d686884b6e621b4ea72f53e6e341954046
hoogamaphone <chris.hoogeboom@gmail.com>
2018-04-08 11:09:16 -0400

    updated the read_html function change the user-agent if the first request fails

--- epub/util.py
+++ epub/util.py
@@ -3,6 +3,7 @@ Utility functions
 """
 import random
 import string
+import urllib.error
 import urllib.request
 
 
@@ -25,8 +26,22 @@ def read_html(url: str) -> str:
     :return: A string containing the HTML of the specified page
     :raise urllib.request.URLError: on errors with opening the page
     """
-    with urllib.request.urlopen(url) as response:
+    print(f'Fetching data from: {url} ...')
+    try:
+        response = urllib.request.urlopen(url)
         html = response.read()
+    except urllib.error.HTTPError as e:
+        if e.code == 403:
+            new_agent = 'not-Python'
+            print(f'Got error code {e.code} because of {e.reason}')
+            print(f'Changing the agent to {new_agent}')
+            request = urllib.request.Request(url)
+            request.add_header('User-Agent', new_agent)
+            with urllib.request.urlopen(request) as response:
+                html = response.read()
+        else:
+            raise e
+    print(f'Successfully read data from: {url}!')
     return html.decode('utf-8')
 
 
